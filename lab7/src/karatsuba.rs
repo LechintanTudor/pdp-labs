846d907d88d96d7ff3b5ca1416d75abd2b29dbb2
Tudor Lechintan <lechintantudor02@gmail.com>
2022-12-16 18:35:47 +0200

    Begin work on distributed Karatsuba

---
+++ lab7/src/karatsuba.rs
@@ -0,0 +1,45 @@
+fn multiply(p1: &[i64], p2: &[i64]) -> Vec<i64> {
+    if p1.len() <= 32 {
+        return simple_multiply(p1, p2);
+    }
+
+    let half_len = p1.len() / 2;
+    let (p1_low, p1_high) = p1.split_at(half_len);
+    let (p2_low, p2_high) = p2.split_at(half_len);
+
+    let p1_sum = p1_low.iter().zip(p1_high.iter()).map(|(c1, c2)| c1 + c2).collect::<Vec<_>>();
+    let p2_sum = p2_low.iter().zip(p2_high.iter()).map(|(c1, c2)| c1 + c2).collect::<Vec<_>>();
+
+    let low_product = multiply(p1_low, p2_low);
+    let high_product = multiply(p1_high, p2_high);
+    let sum_product = multiply(&p1_sum, &p2_sum);
+
+    let middle_product: Vec<i64> = sum_product
+        .iter()
+        .zip(low_product.iter())
+        .zip(high_product.iter())
+        .map(|((s, l), h)| s - l - h)
+        .collect();
+
+    let mut result = vec![0; p1.len() + p2.len() - 1];
+
+    for i in 0..(p1.len() - 1) {
+        result[i] += low_product[i];
+        result[i + half_len] += middle_product[i];
+        result[i + p1.len()] += high_product[i];
+    }
+
+    result
+}
+
+fn simple_multiply(p1: &[i64], p2: &[i64]) -> Vec<i64> {
+    let mut result = vec![0_i64; p1.len() + p2.len() - 1];
+
+    for (i, c1) in p1.iter().enumerate() {
+        for (j, c2) in p2.iter().enumerate() {
+            result[i + j] += c1 * c2;
+        }
+    }
+
+    result
+}
---
+++ lab7/src/simple.rs
@@ -0,0 +1,82 @@
+use crate::mpi_state::MpiState;
+use crate::utils;
+use mpi::traits::*;
+
+pub fn run() {
+    let state = MpiState::default();
+
+    if state.is_master() {
+        let (p1, p2) = utils::generate_polynomials(12);
+
+        let mut result = vec![0_i64; p1.len() + p2.len() - 1];
+        let chunk_size = result.len() / state.slave_proccess_count();
+        let last_chunk_size = chunk_size + result.len() % state.slave_proccess_count();
+
+        // Send standard chunks to all slave proccess but one
+        for i in 0..(state.slave_proccess_count() - 1) {
+            let slave = state.get_slave_proccess(i);
+
+            slave.send(&((i * chunk_size) as u64));
+            slave.send(&(chunk_size as u64));
+            slave.send(&p1);
+            slave.send(&p2);
+        }
+
+        // Send extended chunk to last slave proccess
+        {
+            let slave = state.get_slave_proccess(state.slave_proccess_count() - 1);
+
+            slave.send(&((result.len() - last_chunk_size) as u64));
+            slave.send(&(last_chunk_size as u64));
+            slave.send(&p1);
+            slave.send(&p2);
+        }
+
+        // Receive standard chunks from all but one slave proccess
+        for i in 0..(state.slave_proccess_count() - 1) {
+            state
+                .get_slave_proccess(i)
+                .receive_into(&mut result[(i * chunk_size)..((i + 1) * chunk_size)]);
+        }
+
+        // Receive extended chunk from last slave proccess
+        {
+            let chunk_start = result.len() - last_chunk_size;
+
+            state
+                .get_slave_proccess(state.slave_proccess_count() - 1)
+                .receive_into(&mut result[chunk_start..]);
+        }
+
+        println!("{:?}", result);
+    } else {
+        let (chunk_start, _) = state.master().receive::<u64>();
+        let (chunk_len, _) = state.master().receive::<u64>();
+        let (p1, _) = state.master().receive_vec::<i64>();
+        let (p2, _) = state.master().receive_vec::<i64>();
+
+        let chunk_start = chunk_start as usize;
+        let chunk_len = chunk_len as usize;
+        let chunk_end = chunk_start + chunk_len;
+
+        let chunk = (chunk_start..chunk_end)
+            .map(|r_index| {
+                let p1_index_range = if r_index < p1.len() {
+                    0..(r_index + 1)
+                } else {
+                    (r_index - p2.len() + 1)..p1.len()
+                };
+
+                p1_index_range
+                    .map(|p1_index| {
+                        let c1 = p1[p1_index];
+                        let c2 = p2[r_index - p1_index];
+                        c1 * c2
+                    })
+                    .sum::<i64>()
+            })
+            .collect::<Vec<_>>();
+
+        state.master().send(&chunk);
+    }
+}
